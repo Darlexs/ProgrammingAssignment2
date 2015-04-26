ed653f8fa0107b601f2a1f33be498b8daf189cf7
Darlexs <alexis_aa_93@hotmail.com>
2015-04-26 13:30:25 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Storing an array in the cache.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){  ## Enter a square matrix and stored in the cache.
+  mi <- NULL                             ## Sets a Null value if not calculated the inverse matrix.
+  
+  set <- function(y) {               ## Sets and stores the array entered.
+    x <<- y
+    mi <<- NULL
+  }
+  get <- function() x                     ## Gets the value of the array entered.
+  setsolve <- function(solve) mi <<- solve   ## Establishes and stores the inverse of the matrix entered.
+  getsolve <- function() mi                ## Gets the value of the inverse matrix.
+  
+  list(set = set, get = get,                  ##Stored in a list the above functions.
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## Get the inverse of a matrix stored.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {         ## Take a stored matrix to obtain its inverse.
+  mi <- x$getsolve()                     ##Gets the inverse of the cache.
+  
+  if(!is.null(mi)) {                ## Inverse if already been calculated, returns the inverse
+    message("getting cached data") ## And returns a message.
+    return(mi)                     
+  }
+  
+  data <- x$get()                       ## If the inverse is not computed, obtains the matrix stored
+  mi <- solve(data, ...)                 ## computes the inverse
+  x$setsolve(mi)                         ## Establish and return the inverse matrix.
+  mi
 }
